f9ae02dad3901d87916d9e9154b7dd6f8d3e22ba
sheikhkaheel <sheikhkaheel@gmail.com>
2023-10-21 15:06:04 +0530

    added loop class

--- C Shrp Programing/C Shrp Programing/MainProgram.cs
+++ C Shrp Programing/C Shrp Programing/MainProgram.cs
@@ -9,8 +9,10 @@ namespace C_Shrp_Programing
             Console.WriteLine("I am Executed from Main");
             NameSum Name = new NameSum();
             Table Tbl = new Table();
+            Loop rep = new Loop();
             Tbl.Form();
             Name.Symbol();
+            rep.Continue();
         }
     }
 }
@@ -19,13 +21,13 @@ class NameSum
 {
     public void Symbol()
     {
-        char x1 = 'k';
+        char ض = 'k';
         char x2 = 'a';
         char x3 = 'h';
         char x4 = 'e';
         char x5 = 'e';
         char x6 = 'l';
-        int result = (int)x1 + (int)x2 + (int)x3 + (int)x4 + (int)x5 + (int)x6;
+        int result = (int)ض + (int)x2 + (int)x3 + (int)x4 + (int)x5 + (int)x6;
         Console.WriteLine($"Sum of Kaheel {result} and Unicode symbol {(char)result}");
     }
 }
@@ -38,5 +40,43 @@ class Table
         Console.WriteLine("0\tKaheel\tIllahi Bagh\t20");
         Console.WriteLine("1\tZaid\tIllahi Bagh\t20");
         Console.WriteLine("2\tZubair\tIllahi Bagh\t23");
+    }
+}
+
+class Loop
+{
+    public void Continue()
+    {
+        int x = 10;
+        int y = x++;
+        Console.WriteLine($"Using Post operator x,y {x} and {y}");
+        y = ++x;
+        Console.WriteLine($"Using Pre operator x,y {x} and {y}");
+        Console.WriteLine("Table");
+        int loop = 2, num = 1;
+        while (num <= 10)
+        {
+            int mul = loop * num;
+            Console.WriteLine($"{loop} * {num} = {mul}");
+            num++;
+        }
+        Console.WriteLine("Switch Statement");
+        int check = 1;
+        switch (check)
+        {
+            case 1:
+                Console.WriteLine("One");
+                break;
+            case 2:
+                Console.WriteLine("Two");
+                break;
+            case 3:
+                Console.WriteLine("Three");
+                break;
+            default:
+                Console.WriteLine("Defult");
+                break;
+
+        }
     }
 }
