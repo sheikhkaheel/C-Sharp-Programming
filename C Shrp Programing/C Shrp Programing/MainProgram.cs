1292ebe66fd53dd356c751066d6421b34bf61126
sheikhkaheel <sheikhkaheel@gmail.com>
2023-10-23 18:56:32 +0530

    added star class

--- C Shrp Programing/C Shrp Programing/MainProgram.cs
+++ C Shrp Programing/C Shrp Programing/MainProgram.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace C_Shrp_Programing
 {
@@ -10,9 +11,13 @@ namespace C_Shrp_Programing
             NameSum Name = new NameSum();
             Table Tbl = new Table();
             Loop rep = new Loop();
+            Star ptr = new Star();
+            Time Count = new Time();
             Tbl.Form();
             Name.Symbol();
             rep.Continue();
+            ptr.Pattners();
+            Count.Clock();
         }
     }
 }
@@ -36,6 +41,7 @@ class Table
 {
     public void Form()
     {
+        Console.WriteLine("Table");
         Console.WriteLine("Id\tName\tAddress \tAge");
         Console.WriteLine("0\tKaheel\tIllahi Bagh\t20");
         Console.WriteLine("1\tZaid\tIllahi Bagh\t20");
@@ -52,7 +58,16 @@ class Loop
         Console.WriteLine($"Using Post operator x,y {x} and {y}");
         y = ++x;
         Console.WriteLine($"Using Pre operator x,y {x} and {y}");
-        Console.WriteLine("Table");
+        Console.WriteLine("If Else");
+        int a = 10, b = 12;
+        if (a > b)
+        {
+            Console.WriteLine("A is greater");
+        } else
+        {
+            Console.WriteLine("Bit is greater");
+        }
+        Console.WriteLine("Loop");
         int loop = 2, num = 1;
         while (num <= 10)
         {
@@ -79,4 +94,106 @@ class Loop
 
         }
     }
+}
+
+public class Time
+{
+    public void Clock()
+    {
+        for (int hour = 0; hour <= 23; hour++)
+        {
+            for (int min = 0; min <= 59; min++)
+            {
+                for (int sec = 0; sec <= 59; sec++)
+                {
+                    Console.SetCursorPosition(60,0);
+                    Console.WriteLine($"Time {hour:D2}:{min:D2}:{sec:D2}"); 
+                    Thread.Sleep(1000); 
+                }
+            }
+        }
+    }
+
+}
+
+class Star
+{
+    public void Pattners()
+    {
+        for(int i = 0; i<5; i++)
+        {
+            for(int j = 0; j<i+1; j++)
+            {
+                string str = "* ";
+                Console.Write(str);
+            }
+            Console.WriteLine();
+        }
+
+        Console.WriteLine();
+
+        for (int i = 0; i < 5; i++)
+        {
+            for (int j = i; j < 5; j++)
+            {
+                string str = "* ";
+                Console.Write(str);
+            }
+            Console.WriteLine();
+        }
+
+        Console.WriteLine();
+
+        for (int i = 0; i < 5; i++)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                string space = " ";
+                Console.Write(space);
+            }
+            for (int j = i; j < 5; j++)
+            {
+                string str = "* ";
+                Console.Write(str);
+            }
+            Console.WriteLine();
+        }
+
+        Console.WriteLine();
+
+        for (int i = 0; i < 5; i++)
+        {
+            for (int j = i + 1; j < 5; j++)
+            {
+                string space = " ";
+                Console.Write(space);
+            }
+            for (int j = 0; j < i + 1; j++)
+            {
+                string str = "* ";
+                Console.Write(str);
+            }
+            Console.WriteLine();
+        }
+
+        Console.WriteLine();
+
+        for(int i = 0; i < 5; i++)
+        {
+            for(int j = 0; j < 5; j++)
+            {
+                if (i == 0 || i == 4 || j == 0 || j == 4)
+                {
+                    string str = "* ";
+                    Console.Write(str);
+                } else
+                {
+                    string space = "  ";
+                    Console.Write(space);
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+
 }
