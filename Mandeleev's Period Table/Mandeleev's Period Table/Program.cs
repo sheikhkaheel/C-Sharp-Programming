7fe84f2b5b9f0c358548dfc8ee86c8e19be8b445
sheikhkaheel <sheikhkaheel@gmail.com>
2023-11-10 15:39:20 +0530

    modified Wazawan

---
+++ Mandeleev's Period Table/Mandeleev's Period Table/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace Mandeleev_s_Periodic_Table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I am Executed");
+            try
+            {
+                Console.WriteLine("Element      Atomic No   Atomic Mass");
+                PeriodicTable obj1 = new PeriodicTable("Hydrogen", 1, 1.0079f);
+                Console.WriteLine($"{obj1.ElementName,-12} {obj1.AtomicNumber,-12} {obj1.AtomicMass,-12}");
+
+                PeriodicTable obj2 = new PeriodicTable("Helium", 2, 4.0026f);
+                Console.WriteLine($"{obj2.ElementName,-12} {obj2.AtomicNumber,-12} {obj2.AtomicMass,-12}");
+
+                PeriodicTable obj3 = new PeriodicTable("Lithium", 3, 6.941f);
+                Console.WriteLine($"{obj3.ElementName,-12} {obj3.AtomicNumber,-12} {obj3.AtomicMass,-12}");
+
+                PeriodicTable obj4 = new PeriodicTable("Beryllium", 4, 9.0122f);
+                Console.WriteLine($"{obj4.ElementName,-12} {obj4.AtomicNumber,-12} {obj4.AtomicMass,-12}");
+
+                PeriodicTable obj5 = new PeriodicTable("Boron", 5, 10.81f);
+                Console.WriteLine($"{obj5.ElementName,-12} {obj5.AtomicNumber,-12} {obj5.AtomicMass,-12}");
+
+                PeriodicTable obj6 = new PeriodicTable("Carbon", 6, 12.01f);
+                Console.WriteLine($"{obj6.ElementName,-12} {obj6.AtomicNumber,-12} {obj6.AtomicMass,-12}");
+
+                PeriodicTable obj7 = new PeriodicTable("Nitrogen", 7, 14.01f);
+                Console.WriteLine($"{obj7.ElementName,-12} {obj7.AtomicNumber,-12} {obj7.AtomicMass,-12}");
+
+                PeriodicTable obj8 = new PeriodicTable("Oxygen", 8, 16.00f);
+                Console.WriteLine($"{obj8.ElementName,-12} {obj8.AtomicNumber,-12} {obj8.AtomicMass,-12}");
+
+                PeriodicTable obj9 = new PeriodicTable("Fluorine", 9, 19.00f);
+                Console.WriteLine($"{obj9.ElementName,-12} {obj9.AtomicNumber,-12} {obj9.AtomicMass,-12}");
+
+                PeriodicTable obj10 = new PeriodicTable("Neon", 10, 20.18f);
+                Console.WriteLine($"{obj10.ElementName,-12} {obj10.AtomicNumber,-12} {obj10.AtomicMass,-12}");
+
+                PeriodicTable obj11 = new PeriodicTable("Sodium", 11, 22.99f);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+
+    public class PeriodicTable
+    {
+        public string ElementName { get; set; }
+        public int AtomicNumber { get; set; }
+        public float AtomicMass { get; set; }
+
+        private static int count = 0;
+
+        public PeriodicTable(string elementName, int atomicNumber, float atomicMass)
+        {
+            if (count < 10)
+            {
+                count++;
+                ElementName = elementName;
+                AtomicNumber = atomicNumber;
+                AtomicMass = atomicMass;
+            }
+            else
+            {
+                throw new Exception("Object can't be created beyond the limit.");
+            }
+        }
+    }
+}
--- Wazawan/Wazawan/Program.cs
+++ Wazawan/Wazawan/Program.cs
@@ -8,15 +8,19 @@ namespace Wazawan
         {
             Console.WriteLine("I am Executed");
             TotalMeat tm = new TotalMeat();
-            Veg vg = new Veg();
+            Spices sp = new Spices();
+            NONVeg vg = new NONVeg();
             vg.ExcessFields();
             vg.CalVegs();
             vg.setMeat(); // Call setMeat method to initialize meat value
             tm.CalMeat(vg.meat); // Pass the meat value to CalMeat
+            Console.WriteLine($"Total People \n {tm.people}");
+            sp.Excess();
+            sp.CalSpices();
         }
     }
 
-    public class Veg
+    public class NONVeg
     {
         private int ristaT = 100, gostabaT = 100, kababT = 70, chickenT = 80, tabakNateenT = 100, kormaT = 60, roganJoshT = 70, abbeGoshT = 120;
         private int rista, gostaba, kabab, chicken, tabakNateen, korma, roganJosh, abbeGosh;
@@ -31,7 +35,7 @@ namespace Wazawan
             korma = kormaT * people;
             roganJosh = roganJoshT * people;
             abbeGosh = abbeGoshT * people;
-            Console.WriteLine($"Rista {rista/1000}kg , Gostaba {gostaba/1000}kg Kabab {kabab/1000}kg, chicken {chicken/1000}kg, TabakNateen {tabakNateen/1000}kg, Korma {korma/1000}kg , roganJosh {roganJosh/1000}kg , AbbeJosh {abbeGosh/1000}kg");
+            Console.WriteLine($"NonVeg \n Rista {rista/1000}kg, Gostaba {gostaba/1000}kg, Kabab {kabab/1000}kg, chicken {chicken/1000}kg, TabakNateen {tabakNateen/1000}kg, Korma {korma/1000}kg , roganJosh {roganJosh/1000}kg , AbbeJosh {abbeGosh/1000}kg");
         }
 
         public void ExcessFields()
@@ -44,23 +48,48 @@ namespace Wazawan
         public void setMeat()
         {
             meat = ristaT + gostabaT + kababT + chickenT + tabakNateenT + kormaT + roganJoshT + abbeGoshT;
-            Console.WriteLine($"Meat per Trame {meat}g");
+            Console.WriteLine($"Meat per Trame \n {meat}g");
         }
     }
 
+    public class Spices
+    {
+        private float fennelSeeds, dryGinger, cloves, cinnamon, cardamom, asafoetida, redChiliPowder, turmeric;
+        private float fennelSeedsT = 2, dryGingerT = 3, clovesT = 1, cinnamonT = 1.5f, cardamomT = 2, asafoetidaT = 2, redChiliPowderT = 6, turmericT = 6;
+        private int people;
+
+        public void Excess()
+        {
+            TotalMeat totalM = new TotalMeat();
+            this.people = totalM.people;
+        }
+        public void CalSpices()
+        {
+            fennelSeeds = people * fennelSeedsT;
+            dryGinger = people * dryGingerT;
+            cloves = people * clovesT;
+            cinnamon = people * cinnamonT;
+            cardamom = people * cardamomT;
+            asafoetida = people * asafoetidaT;
+            redChiliPowder = people * redChiliPowderT;
+            turmeric = people * turmericT;
+            Console.WriteLine($"Spices \n FennelSeeds {fennelSeeds/1000}kg, DryGinger {dryGinger/1000}kg, Cloves {cloves/1000}kg, Cinnamon {cinnamon/1000}kg,  Cardamom {cardamom/1000}kg, Asafoetida {asafoetida/1000}kg, RedChiliPowder {redChiliPowder/1000}kg, Turmeric {turmeric/1000}kg");
+        }
+    }
     public class TotalMeat
     {
-        public int people = 100;
+        public int people = 400;
 
-        public void CalTrame()
-        {
+        public void CalTrame() 
+        { 
+        
             int trames = people / 4;
         }
 
         public void CalMeat(int vegMeat) // Accept the meat value as a parameter
         {
             int totalMeat = vegMeat * people;
-            Console.WriteLine($"Total Meat {totalMeat/1000}kg");
+            Console.WriteLine($"Total Meat \n {totalMeat/1000}kg");
         }
     }
 }
