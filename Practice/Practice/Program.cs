6d4201b5a86e3a5ec84c445ab8f7bf193fe5d6e8
sheikhkaheel <sheikhkaheel@gmail.com>
2023-11-12 18:33:56 +0530

    added a new project

---
+++ Practice/Practice/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Practice Programming");
+            B obj2 = new B();
+            obj2.add();
+        }
+    }
+}
+
+public class A
+{
+    public int Number { get; set; }
+    public A()
+    {
+        Console.WriteLine("Constructer A");
+    }
+    public void add()
+    {
+        Console.WriteLine("A Method Called");
+    }
+}
+
+public class B:A
+{
+    public B()
+    {
+        Console.WriteLine("Constructer B");
+    }
+
+    public void add()
+    {
+        Console.WriteLine("B Method Called");
+    }
+}
